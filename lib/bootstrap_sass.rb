a0d2303150c91fc6a64ed0cd03a39761ce680595
Pavel Pachkovskij <pavel.pachkovskij@gmail.com>
2014-10-30 16:40:50 +0300

    add fonts to precompile

--- lib/bootstrap_sass.rb
+++ lib/bootstrap_sass.rb
@@ -3,6 +3,12 @@ require "bootstrap_sass/version"
 module BootstrapSass
   module Rails
     class Engine < ::Rails::Engine
+      initializer 'bootstrap-sass.assets.precompile' do |app|
+        %w(stylesheets javascripts fonts).each do |sub|
+          app.config.assets.paths << root.join('assets', sub).to_s
+        end
+        app.config.assets.precompile << %r(bootstrap/glyphicons-halflings-regular\.(?:eot|svg|ttf|woff)$)
+      end
     end
   end
 end
